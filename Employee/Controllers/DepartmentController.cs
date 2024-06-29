e7fcfc7370740ac770eeeb4e5f417be32351c5b2
Asad2730 <asadsajjad777@gmail.com>
2024-06-29 18:16:08 +0500

    ...

---
+++ Employee/Controllers/DepartmentController.cs
@@ -0,0 +1,119 @@
+﻿using Employee.Data;
+using Employee.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Employee.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+
+        private readonly AppDbContext db;
+
+        public DepartmentController(AppDbContext db)
+        {
+            this.db = db;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Department>>> GetAll()
+        {
+            try
+            {  var q = await db.Departments.ToListAsync();
+                return Ok(q);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Department>> Get(int id)
+        {
+            try
+            {
+                var q = await db.Departments.FirstOrDefaultAsync(i => i.Id == id);
+                if (q == null)
+                {
+                    return NotFound("Department Not found");
+                }
+
+                return Ok(q);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+
+        }
+
+
+
+        [HttpPost]
+        public async Task<ActionResult<Department>> Create(Department department)
+        {
+            try
+            {
+                db.Departments.Add(department);
+                await db.SaveChangesAsync();
+                return CreatedAtAction(nameof(Get), new { id = department.Id }, department);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update(int id, Department department)
+        {
+            try
+            {
+                if (id != department.Id)
+                {
+                    return NotFound("Department not found");
+                }
+
+                db.Entry(department).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return Ok("Department updated");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+
+
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            try
+            {
+                var q = await db.Departments.FirstOrDefaultAsync(x => x.Id == id);
+                if (q == null)
+                {
+                    return NotFound();
+                }
+
+                db.Departments.Remove(q);
+                await db.SaveChangesAsync();
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+    }
+}
---
+++ Employee/Controllers/UserController.cs
@@ -0,0 +1,116 @@
+﻿using Employee.Data;
+using Employee.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Employee.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        private readonly AppDbContext db;
+
+        public UserController(AppDbContext db) {
+            this.db = db;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetAll()
+        {
+            try
+            {   
+                var q = await db.Users.Include(e => e.Department.Name).ToListAsync();
+                return Ok(q);
+            }
+            catch (Exception ex) { 
+
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> Get(int id)
+        {
+            try
+            {
+                var q = await db.Users.Include(i => i.Department.Name).FirstOrDefaultAsync(i => i.Id == id);
+                if (q == null)
+                {
+                    return NotFound("User Not found");
+                }
+
+                return Ok(q);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+
+        }
+
+
+
+        [HttpPost]
+        public async Task<ActionResult<User>> Create(User user)
+        {
+            try
+            {
+                db.Users.Add(user);
+                await db.SaveChangesAsync();
+                return CreatedAtAction(nameof(Get),new {id =  user.Id},user);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update(int id, User user)
+        {
+            try
+            {  
+                if( id != user.Id)
+                {
+                    return NotFound("User not found");
+                }
+
+                db.Entry(user).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return Ok("User updated");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+
+
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            try
+            {
+               var q = await db.Users.FirstOrDefaultAsync(x => x.Id == id);
+                if(q == null)
+                {
+                    return NotFound();
+                }
+
+                db.Users.Remove(q);
+                await db.SaveChangesAsync();
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+    }
+}
